9d179742e5ea7d2c4d43b545637a2a8eddfd2fbe
Kevin Schiesser <bfb@riseup.net>
2015-03-14 10:52:57 -0700

    example for run that takes a float, not arrays

--- example_test.go
+++ example_test.go
@@ -5,16 +5,17 @@ import (
 )
 
 func ExampleFilter_Run() {
+	design := NewFilterDesign("HpBeZ1/2", 4)
+	filt := NewFilter(design)
+
 	in := make([]float64, 4)
 	out := make([]float64, 4)
 	for i := range in {
 		in[i] = float64(i)
+		out[i] = filt.Run(in[i])
 	}
 
-	design := NewFilterDesign("HpBeZ1/2", 4)
-	filt := NewFilter(design)
-
-	filt.Run(in, out)
+	filt.Close()
 
 	fmt.Println("IN:", in)
 	fmt.Println("OUT:", out)
